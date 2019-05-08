27e934eced1c9efcea8f4bce1de7e00978387a4e
Ahmed Hassan <ahmdmhasn@icloud.com>
2019-05-08 21:54:36 +0200

    Initial Commit

---
+++ Et3amTests/Et3amTests.swift
@@ -0,0 +1,36 @@
+//
+//  Et3amTests.swift
+//  Et3amTests
+//
+//  Created by Ahmed M. Hassan on 5/8/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import XCTest
+@testable import Et3am
+
+class Et3amTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
