eb6947a23ebdaf1a91ab1cfab653181d4a0599dc
Mohamed Korany Ali <MohamedKorany@learniosfreecoms-mac.local>
2019-05-14 14:38:51 +0200

    ResturantInfo

--- Et3am/RestaurantInfoViewController.swift
+++ Et3am/RestaurantInfoViewController.swift
@@ -10,66 +10,9 @@ import UIKit
 
 class ResturantDetails : UIViewController{
     
-    @IBOutlet weak var textBox: UITextField!
-    @IBOutlet weak var dropdown: UIPickerView!
-     var coupons = ["50","100","150","200"]
-        
-    
-    
     override func viewDidLoad() {
-        super.viewDidLoad()
-coupons = ["50","100","150","200"]
-        // Do any additional setup after loading the view.
-    }
-    
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        var countrows : Int = coupons.count
-        if pickerView == dropdown {
-            
-            countrows = self.coupons.count
-        }
-        
-        return countrows
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == dropdown {
-            
-            let titleRow = coupons[row]
-            
-            return titleRow
-            
-        }
-            
-        else if pickerView == dropdown{
-            let titleRow = coupons[row]
-            
-            return titleRow
-        }
-        
-        return ""
-    }
-    
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == dropdown {
-            self.textBox.text = self.coupons[row]
-            self.dropdown.isHidden = true
-        }
-            
         
     }
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if (textField == self.textBox){
-            self.dropdown.isHidden = false
-            
-        }
-       
-        
-    }
-
     
 
 }
