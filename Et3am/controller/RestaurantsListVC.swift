d08740fd216f7136960a79ff90380215955e7590
Mohamed  Korany <mohamed.korany.ali@learniosfreecoms-mac.local>
2019-07-02 23:45:30 +0300

    update parsing son and add search bar

--- Et3am/controller/RestaurantsListVC.swift
+++ Et3am/controller/RestaurantsListVC.swift
@@ -11,7 +11,11 @@ import CoreLocation
 import SVProgressHUD
 import SDWebImage
 
-class RestaurantsListVC: UITableViewController {
+class RestaurantsListVC: UITableViewController ,UISearchBarDelegate{
+    
+    @IBOutlet weak var SearchBarInTable: UISearchBar!
+    var filteredData = [Restaurant]()
+    var inSearchMode = false
     
 
     var restaurantsList = [Restaurant]()
@@ -31,6 +35,8 @@ class RestaurantsListVC: UITableViewController {
     }
     
     override func viewDidLoad() {
+        SearchBarInTable.delegate = self
+
         super.viewDidLoad()
         noList.center = view.center
         //noList.text = "You don't have any used coupon."
@@ -56,27 +62,82 @@ class RestaurantsListVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         print("numberOfRowsInSection \(restaurantsList.count)")
-        return restaurantsList.count;
+        if inSearchMode {
+            
+            return filteredData.count
+        }
+        else{
+            
+            return restaurantsList.count;}
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueCell() as RestaurantCell
+        if inSearchMode {
+            
+            drawCell(cell: cell, selectedList: filteredData, indexPath: indexPath)
         
-        let placeholderImage = UIImage(named: "placeholder")!
-        let imageURL = ImageAPI.getImage(type: .width150, publicId: restaurantsList[indexPath.row].image ?? "")
-        cell.restaurantImage.sd_setShowActivityIndicatorView(true)
-        cell.restaurantImage.sd_setImage(with: URL(string: imageURL), placeholderImage: placeholderImage, options: [], completed: nil)
-        cell.restaurantNameLabel.text = restaurantsList[indexPath.row].restaurantName
-        cell.restaurantDistanceLabel.text = String(describing: (restaurantsList[indexPath.row].distance)!)+" km"
-        cell.restaurantTimeLabel.text = String(describing: (restaurantsList[indexPath.row].travelTime)!)+" min"
+            
+        }
+            
+            
+        else{
+        
+            drawCell(cell: cell, selectedList: restaurantsList, indexPath: indexPath)
+        
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailsView : RestaurantDetailsViewController = storyboard?.instantiateViewController(withIdentifier: "RestaurantDetailsVCID") as! RestaurantDetailsViewController
+        
+        
+        if inSearchMode{
+        detailsView.restuarantObj = self.filteredData[indexPath.row]
+        }
+        else{
         detailsView.restuarantObj = self.restaurantsList[indexPath.row]
+        }
         self.navigationController?.pushViewController(detailsView, animated: true);
     }
+    func drawCell(cell:RestaurantCell ,selectedList:Array<Restaurant> , indexPath: IndexPath)  {
+        let placeholderImage = UIImage(named: "placeholder")!
+        let imageURL = ImageAPI.getImage(type: .width150, publicId: selectedList[indexPath.row].image ?? "")
+        cell.restaurantImage.sd_setShowActivityIndicatorView(true)
+        cell.restaurantImage.sd_setImage(with: URL(string: imageURL), placeholderImage: placeholderImage, options: [], completed: nil)
+        cell.restaurantNameLabel.text = selectedList[indexPath.row].restaurantName
+        cell.restaurantDistanceLabel.text = String(describing: (selectedList[indexPath.row].distance)!)+" km"
+        cell.restaurantTimeLabel.text = String(describing: (selectedList[indexPath.row].travelTime)!)+" min"
+    }
     
+}
+extension RestaurantsListVC {
+
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            
+            filteredData = restaurantsList
+            inSearchMode = false
+            tableView.reloadData()
+            
+        }
+            
+        else {
+            
+            filteredData = restaurantsList.filter({($0.restaurantName?.lowercased().contains(searchText.lowercased()))! })
+            inSearchMode = true
+            tableView.reloadData()
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        inSearchMode = false
+        searchBar.text = ""
+        tableView.reloadData()
+    }
+
+
 }
