da5251acccd761bef65d6a5b115c7b6194f2b7b9
elmahask <welmahask@gmail.com>
2019-07-16 00:44:58 +0300

    Rearrange File in RestaurantsList

---
+++ Et3am/view/RestaurantList/RestaurantCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantCell.swift
+//  Et3am
+//
+//  Created by Wael M Elmahask on 9/23/1440 AH.
+//  Copyright © 1440 AH Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    @IBOutlet weak var restaurantImage: UIImageView!
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var restaurantDistanceLabel: UILabel!
+    @IBOutlet weak var restaurantTimeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
