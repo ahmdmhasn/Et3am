c48cd6354f15712e9950e261dc3b37653f082d18
elmahask <welmahask@gmail.com>
2019-07-16 00:44:09 +0300

    Rearrange File in Register

---
+++ Et3am/view/RegisterLoginScreen/SignInView.swift
@@ -0,0 +1,17 @@
+//
+//  signInView.swift
+//  Et3am
+//
+//  Created by Jets39 on 5/14/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class SignInView: UIStackView {
+    
+    @IBOutlet weak var emailTxtField :UITextField!
+    @IBOutlet weak var passTxtField :UITextField!
+    @IBOutlet weak var valdiatelabel:UILabel!
+    
+}
