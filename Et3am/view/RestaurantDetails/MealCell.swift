0d9f31a996d7d1cd797d3065853262673f505fce
elmahask <welmahask@gmail.com>
2019-07-16 00:41:55 +0300

    Rearrange File in RestaurantDetails

---
+++ Et3am/view/RestaurantDetails/MealCell.swift
@@ -0,0 +1,22 @@
+//
+//  MealCell.swift
+//  Et3am
+//
+//  Created by Mohamed Korany Ali on 9/9/1440 AH.
+//  Copyright © 1440 AH Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class MealCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var mealValue: UILabel!
+    @IBOutlet weak var mealName: UILabel!
+    @IBOutlet weak var mealImage: UIImageView!
+
+    
+}
+
+
---
+++ Et3am/view/RestaurantDetails/RestaurantDetailsCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailsCell.swift
+//  Et3am
+//
+//  Created by Mohamed Korany Ali on 9/21/1440 AH.
+//  Copyright © 1440 AH Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailsCell: UITableViewCell {
+    weak var delegate: RestaurantDetailsViewController?
+    var indexPath:IndexPath!
+    @IBOutlet weak var restaurantImage: UIImageView!
+    @IBOutlet weak var restaurantName: UILabel!
+    @IBOutlet weak var restaurantCountyCity: UILabel!
+    @IBOutlet weak var mapImageView: UIImageView!
+    
+    
+}
+protocol RestaursntCellDelegate: class {
+    func tapedImage()
+}
+
